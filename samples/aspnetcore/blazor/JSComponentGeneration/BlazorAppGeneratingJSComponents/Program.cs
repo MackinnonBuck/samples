6a1b5545f4e810d7cd2ff6d08651b0364d7a7156
Mackinnon Buck <mackinnon.buck@gmail.com>
2021-09-13 16:07:03 -0700

    Added sample using JS api

--- samples/aspnetcore/blazor/JSComponentGeneration/BlazorAppGeneratingJSComponents/Program.cs
+++ samples/aspnetcore/blazor/JSComponentGeneration/BlazorAppGeneratingJSComponents/Program.cs
@@ -5,9 +5,11 @@ using System;
 using JSComponentGeneration.Angular;
 using JSComponentGeneration.React;
 using BlazorAppGeneratingJSComponents;
+using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.RegisterForAngular<TodoItem>();
+//builder.RootComponents.RegisterForAngular<TodoItem>();
+builder.RootComponents.RegisterForJavaScript<TodoItem>("TodoItem");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
